0023b88ed2f70f235d47b5624b05b42329ddacd2
Hiren Lakhatariya <89458119+Hiren-Lakhatariya@users.noreply.github.com>
2022-04-17 19:39:46 +0530

    Update Lab_8_2.java

--- Lab_8/Lab_8_2.java
+++ Lab_8/Lab_8_2.java
@@ -2,34 +2,41 @@ package com.company;
 
 import java.util.Scanner;
 
-public class Lab_8_2 {
-    public static void main(String[] args)
-    {
+public class Practice {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter hour and minute for Time1 :");
-        int h1 = sc.nextInt();
-        int m1 = sc.nextInt();
-        System.out.println("enter hour and minute for Time2 :");
-        int h2 = sc.nextInt();
-        int m2 = sc.nextInt();
-        Time t =new Time();
-       t.add(h1,m1,h2,m2);
+        System.out.println("Enter first time");
+        int h1= sc.nextInt();
+        int m1= sc.nextInt();
+        System.out.println("Enter second time");
+        int h2= sc.nextInt();
+        int m2= sc.nextInt();
+        Time1 t1 = new Time1(h1,m1);
+        Time1 t2 = new Time1(h1,m2);
+        Time1 t = new Time1();
+        t.add(t1,t2);
+        System.out.println(t.hour + ":" + t.minute);
     }
 }
-class Time
+class Time1
 {
     int hour;
     int minute;
-    void add(int h1, int m1, int h2, int m2)
+    public Time1(int hour,int minute)
     {
-        hour = h1+h2;
-        minute = m1+m2;
-        if(minute>=60)
-        {
-            minute=minute%60;
-            hour++;
-        }
-        System.out.println(hour+":"+minute +" hours");
+        this.hour=hour;
+        this.minute= minute;
     }
+    public Time1(){}
+    public void add(Time1 t1,Time1 t2)
+    {
+        this.hour= t1.hour+ t2.hour;
+        this.minute= t1.minute+t2.minute;
 
+        if(this.minute>=60)
+        {
+            this.hour=this.hour+this.minute/60;
+            this.minute=this.minute%60;
+        }
+    }
 }
